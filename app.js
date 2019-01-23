80ee825a9a6705fbd9facc34dce382eb3189b3e6
DinnerKang <jenhyuk0318@gmail.com>
2019-01-23 17:26:07 +0900

    add rest api

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!\n');
- });
+app.post('/users/register', (req, res) =>{
+  // 유저 등록
+});
+app.delete('/users/:id', (req, res) =>{
+  // 유저 삭제
+});
+app.post('/auth/login', (req, res) =>{
+  // 로그인 인증
+});
+app.get('/auth/check', (req, res) =>{
+  // 인증 확인
+})
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('3000 포트 열기 !');
 });
