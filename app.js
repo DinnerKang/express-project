2f5fe8c3462893f06c884f15d164037143e21fb3
DinnerKang <jenhyuk0318@gmail.com>
2019-01-31 16:49:35 +0900

    update regist

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const dbConfig = require('./config');
 const connection = mysql.createConnection(dbConfig.SQL);
 
+let User = require('./models/user/user');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -23,17 +25,54 @@ app.get('/', (req, res) => {
 });
 
 app.post('/users/register', (req, res) => {
-  const id = req.params.id;
-  const pwd = req.params.pwd;
-  const role = req.params.role;
-  // 유저 등록
-  connection.query(`INSERT INTO user (user_id, user_pwd, user_role) VALUES ("${id})", "${pwd}", "${role}"`,
-    function(error, results, fields){
+  User.user_id = req.body.id;
+  User.user_pwd = req.body.pwd;
+  User.user_role = req.body.role;
 
+  // 유저 등록
+  if (User.user_id && User.user_pwd && User.user_role) {
+    connection.query(`SELECT user_id FROM user WHERE user_id = "${id}"`, function (error, check_result, fields) {
+      if (check_result.length == 0) {
+        connection.query(`INSERT INTO user (user_id, user_pwd, user_role) VALUES ("${User.user_id }", "${User.user_pwd}", "${User.user_role}")`,
+          function (error, results, fields) {
+            if (error) {
+              console.log(error);
+            }
+            res.status(200).json({
+              'status': 200,
+              'msg': 'success'
+            });
+          });
+      } else {
+        res.status(400).json({
+          'status': 400,
+          'msg': '중복 ID'
+        });
+      }
     });
+  } else {
+    res.status(400).json({
+      'status': 400,
+      'msg': '값을 다 채워주세요'
+    });
+  }
+
 });
 app.delete('/users/:id', (req, res) => {
   // 유저 삭제
+  User.user_id = req.body.id;
+  connection.query(`DELETE FROM user WHERE user_id = "${id}"`, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    }
+    if (results.length == 0) {
+      console.log('찾는값 없음');
+      return res.status(400).json({
+        error: 'Incorrect id'
+      });
+    }
+    res.status(201).send('success');
+  });
 });
 app.post('/auth/login', (req, res) => {
   // 로그인 인증
